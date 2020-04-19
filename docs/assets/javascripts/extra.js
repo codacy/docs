347c434473c088fe80ca09f21c53ad78ee0f2eb9
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2020-04-19 18:47:09 +0100

    Fix JavaScript issues identified by Codacy

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -6,12 +6,12 @@ window.intercomSettings = {
 // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/a53cb793563c5f92959598bf8b22fe8f01ec3199'
 (function () { var w = window; var ic = w.Intercom; if (typeof ic === "function") { ic('reattach_activator'); ic('update', w.intercomSettings); } else { var d = document; var i = function () { i.c(arguments); }; i.q = []; i.c = function (args) { i.q.push(args); }; w.Intercom = i; var l = function () { var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/a53cb793563c5f92959598bf8b22fe8f01ec3199'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); }; if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); } } })();
 
-document.addEventListener("DOMContentLoaded", function() {
-    show_toc_left();
-});
-
-function show_toc_left(){
-    document.querySelectorAll('.md-nav .md-nav--secondary')[0].setAttribute("style", "display: block; overflow: visible; color: #7d7f8e9c")
-    document.querySelectorAll('.md-nav .md-nav--secondary label')[0].remove()
-    document.querySelectorAll('.md-sidebar.md-sidebar--secondary')[0].remove()
+function showTOCLeft(){
+    document.querySelectorAll(".md-nav .md-nav--secondary")[0].setAttribute("style", "display: block; overflow: visible; color: #7d7f8e9c");
+    document.querySelectorAll(".md-nav .md-nav--secondary label")[0].remove();
+    document.querySelectorAll(".md-sidebar.md-sidebar--secondary")[0].remove();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    showTOCLeft();
+});
