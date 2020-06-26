dc18b304774ebd419fe730e0fb0acd35abc54200
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2020-06-26 09:23:08 +0000

    Removed v1.1.0 with mike 0.5.1

--- v1.1.0/Chart/infrastructure/AKS/backend/variables.tf
+++
@@ -1,7 +0,0 @@
-# variables.tf - user settings. For provider settings see config.tf
-
-variable "project_tag" {
-  description = "Project tag to add to s3 and dynamodb names"
-  type = string
-  default = "codacy"
-}
