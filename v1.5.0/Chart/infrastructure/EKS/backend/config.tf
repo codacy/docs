e0f91d46034148619c36464e432d9c760e65ecf4
pauloribeiro-codacy <pauloribeiro-codacy@users.noreply.github.com>
2020-07-22 20:12:37 +0000

    Deployed 5faf0f84 to v1.5.0 with MkDocs 1.1 and mike 0.5.1

---
+++ v1.5.0/Chart/infrastructure/EKS/backend/config.tf
@@ -0,0 +1,17 @@
+# config.tf - terraform and providers configuration
+
+terraform {
+  required_version = "~> 0.12"
+}
+
+provider "aws" {
+  # Set your AWS configuration here. For more information see the terraform
+  # provider information: https://www.terraform.io/docs/providers/aws/index.html
+  # You might need to set AWS_SDK_LOAD_CONFIG=1 to use your aws credentials file
+  region  = "eu-west-1"
+  version = "~> 2.33"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
