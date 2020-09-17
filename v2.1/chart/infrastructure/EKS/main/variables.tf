b30cbad7793fdfda256a04f925e54f8baafae8f5
pauloribeiro-codacy <pauloribeiro-codacy@users.noreply.github.com>
2020-09-17 15:16:27 +0000

    Deployed 1928cbd0 to v2.1 with MkDocs 1.1.2 and mike 0.5.3

---
+++ v2.1/chart/infrastructure/EKS/main/variables.tf
@@ -0,0 +1,134 @@
+# variables.tf - user settings. For terraform settings see config.tf
+
+### project
+variable "project_name" {
+  description = "Base project name, used to tag resources"
+  type        = string
+  default     = "Codacy"
+}
+
+variable "project_slug" {
+  description = "Base project slug, used to name resources"
+  type        = string
+  default     = "codacy"
+}
+
+variable "custom_tags" {
+  description = "Map of custom tags to apply to every resource"
+  type        = map(string)
+  default     = {}
+}
+
+### kubernetes
+variable "k8s_version" {
+  description = "Kubernetes version to use in the cluster"
+  type        = string
+  default     = "1.14"
+}
+
+variable "k8s_worker_type" {
+  description = "Instance type used for kubernetes worker nodes"
+  type        = string
+  default     = "m5.2xlarge"
+}
+
+variable "k8s_worker_bootstrap_extra_flags" {
+  description = "Additional flags to pass to the kubernetes worker node bootstrap script"
+  type        = string
+  default     = ""
+}
+
+variable "k8s_worker_disk_size" {
+  description = "Size (in GB) of the disk used for kubernetes worker nodes"
+  type        = number
+  default     = 100
+}
+
+variable "k8s_worker_min" {
+  description = "Minimimum number of kubernetes worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "k8s_worker_max" {
+  description = "Maximum number of kubernetes worker nodes"
+  type        = number
+  default     = 8
+}
+
+variable "k8s_worker_desired" {
+  description = "Desired number of kubernetes worker nodes"
+  type        = number
+  default     = 4
+}
+
+### network
+variable "create_network_stack" {
+  description = "If true then create the network stack (set to false to use preexisting VPC / networks)"
+  type        = bool
+  default     = true
+}
+
+### create everything
+variable "vpc_cidr" {
+  description = "CIDR of the VPC wich house the EKS cluster"
+  type        = string
+  default     = "10.8.0.0/16"
+}
+
+variable "private_subnet1_cidr" {
+  description = "CIDR of private subnet 1 (this should be large)"
+  type        = string
+  default     = "10.8.0.0/18"
+}
+
+variable "private_subnet2_cidr" {
+  description = "CIDR of private subnet 2 (this should be large)"
+  type        = string
+  default     = "10.8.64.0/18"
+}
+
+variable "public_subnet1_cidr" {
+  description = "CIDR of public subnet 1"
+  type        = string
+  default     = "10.8.128.0/19"
+}
+
+variable "public_subnet2_cidr" {
+  description = "CIDR of public subnet 2"
+  type        = string
+  default     = "10.8.160.0/19"
+}
+
+variable "create_vpc_endpoints" {
+  description = "If true then vpc endpoints for DynamoDB and S3"
+  # This allows direct access to these resources from the private subnet, thus reducing nat gateway trafic.
+  type    = bool
+  default = false
+}
+
+### use existing vpc and subnets (create_network_stack == false)
+variable "vpc_id" {
+  description = "ID of the vpc where to deploy (only used if create_network_stack == false)"
+  type        = string
+  default     = null
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs to use for the cluster (only used if create_network_stack == false)"
+  type        = list(string)
+  default     = []
+}
+
+### general
+variable "ssm_prefix" {
+  description = "Prefix used for the SSM parameters"
+  type        = string
+  default     = "/codacy/enterprise"
+}
+
+variable "enable_ssm" {
+  description = "If true then add systems manager policy to nodes (this allows remote access for authorized users)"
+  type        = bool
+  default     = false
+}
---
+++ v2.1/chart/troubleshoot/extract-codacy-logs.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+#### Helper functions ####
+
+usage()
+{
+    echo "Usage: $0 -n <NAMESPACE> [-d <DAYS_OF_LOGS>]"
+    echo "When using the -d flag, <DAYS_OF_LOGS> must be between 1 and 7"
+    echo "Example: '$0 -n codacy -d 3' would retrieve the last 3 days of logs from the 'codacy' namespace of the kubernetes cluster"
+    exit 3
+}
+
+date_days_ago() {
+    OS=$(uname)
+    if [[ "$OS" == "Darwin" ]]; then
+        date -v -$1\d "+%Y-%m-%d"
+    elif [[ "$OS" == "Linux" ]]; then
+        date -d '-$1 day' "+%Y-%m-%d"
+    else
+       echo "Unsupported operating system '$OS'"
+       exit 11
+    fi
+}
+
+cleanup()
+{
+    echo "Cleaning temporary files, exiting..."
+    if [ -d "$LOGS_DIR" ]; then
+        rm -r $LOGS_DIR &>/dev/null
+    fi
+}
+trap cleanup EXIT
+
+#### Log extraction script ####
+
+while getopts "n:d:" option; do
+    case ${option} in
+        n )
+            NAMESPACE=${OPTARG}
+            ;;
+        d )
+            if [ $OPTARG -le 0 ] || [ $OPTARG -ge 8 ]; then
+                usage
+            else
+                DAYS=$((${OPTARG}-1)) # Day 0 is today
+            fi
+            ;;
+        \? )
+            usage
+            ;;
+    esac
+done
+
+[ -z "$NAMESPACE" ] && usage
+
+CURRENT_DATE_TIME=$(date "+%Y%m%d-%H%M%S")
+LOGS_DIR="$(mktemp -d)/codacy_logs_$CURRENT_DATE_TIME"
+
+echo "Starting log files extraction"
+
+# Check if zip is available
+echo "Checking if zip is installed..."
+if ! zip --version &>/dev/null; then
+    echo "zip not installed"
+    echo "Please install zip"
+    exit 4
+fi
+
+
+# Check if kubectl or microk8s.kubectl is available
+echo "Checking if kubectl is installed..."
+KUBECTL=$(which kubectl || which microk8s.kubectl)
+if [ $? -ne 0 ]; then
+    echo "kubectl not installed"
+    echo "Please install kubectl version specified in Codacy's documentation (see here - https://codacy.github.io/chart/install/) or the version used when installing your cluster"
+    echo "To install kubectl see https://kubernetes.io/docs/tasks/tools/install-kubectl/ (or https://microk8s.io/docs/ if you are running a microk8s kubernetes cluster)"
+    exit 4
+fi
+
+# Check current cluster context
+echo "Checking access to kubernetes cluster..."
+KUBE_CTX=$($KUBECTL config current-context)
+if [ $? -ne 0 ]; then
+    echo "No kubernetes cluster context configured"
+    exit 5
+fi
+
+read -p "Is '$KUBE_CTX' the correct kubernetes cluster for log extraction? (yes/[no]): " ANSWER
+if [[ ! "$ANSWER" =~ ^y(es)?$ ]]; then
+    echo "Please configure correctly your current kubernetes cluster"
+    exit 6
+fi
+
+# Create temporary directory for copying logs
+if ! mkdir -p $LOGS_DIR; then
+    echo "Failed to create temporary directory $LOGS_DIR , for log files extraction"
+    exit 7
+fi
+
+# Get pod name for the logs pod
+echo "Checking if the logs kubernetes pod exists..."
+LOGS_POD_NAME=$($KUBECTL get pods -n $NAMESPACE -l app=minio -o jsonpath='{.items[*].metadata.name}')
+if [ $? -ne 0 ]; then
+    echo "Failed to get the name of the logs kubernetes pod, for namespace $NAMESPACE"
+    echo "Are you sure you are in the right kubernetes cluster context?"
+    exit 8
+fi
+
+# Copy logs to local filesystem
+echo "Extracting log files..."
+if ! $KUBECTL cp $NAMESPACE/$LOGS_POD_NAME:/export/logs $LOGS_DIR; then
+    echo "Failed to extract log files from kubernetes pod $LOGS_POD_NAME to local directory $LOGS_DIR"
+    echo "Are you sure you are in the right kubernetes cluster context?"
+    exit 9
+fi
+
+# Get descriptions for all the pods
+KUBECTL_DESCRIBE_FILE_NAME="describe_pods.txt"
+echo "Getting kubectl describe for all codacy pods..."
+if ! $KUBECTL describe pods -n $NAMESPACE > $LOGS_DIR/$KUBECTL_DESCRIBE_FILE_NAME; then
+    echo "Failed to save the codacy pods description to local directory $LOGS_DIR"
+    echo "Are you sure you are in the right kubernetes cluster context?"
+    exit 10
+fi
+
+echo "Compressing extracted log files..."
+if [ -n "$DAYS" ]; then
+
+    # Incrementally generate dates with the format specified below, for each day of logs, to make pattern matching easier
+    while [ $DAYS -ge 0 ]; do
+        LOGS_DATE=$(date_days_ago $DAYS)
+
+        # Find all log files that match the pattern and add them to the ZIP archive (-9 is maximum, slowest, compression)
+        find $LOGS_DIR -iname $LOGS_DATE\* -o -iname $KUBECTL_DESCRIBE_FILE_NAME -type f | xargs -L 10 zip -ur9 codacy_logs_$CURRENT_DATE_TIME.zip
+
+        # Incremental zipping might fail
+        if [ $? -ne 0 ]; then
+            echo "Failed to compress logs (located in $LOGS_DIR) to a ZIP file"
+            echo "If this step continues to fail, you can compress the files manually"
+            exit 11
+        fi
+
+        DAYS=$(($DAYS - 1))
+    done
+
+else
+    # Compress logs in ZIP file (-9 is maximum, slowest, compression)
+    if ! zip -r9 codacy_logs_$CURRENT_DATE_TIME.zip $LOGS_DIR; then
+        echo "Failed to compress logs (located in $LOGS_DIR) to a ZIP file"
+        echo "If this step continues to fail, you can compress the files manually"
+        exit 11
+    fi
+fi
+
+echo "Log file extraction completed"
