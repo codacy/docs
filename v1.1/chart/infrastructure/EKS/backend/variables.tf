7f22a79057ac585213d9a8fcd05d4c03fd8955f5
pauloribeiro-codacy <pauloribeiro-codacy@users.noreply.github.com>
2020-11-17 16:48:48 +0000

    Deployed 21f9b2c5 to v1.1 with MkDocs 1.1.2 and mike 0.5.3

--- v1.1/chart/infrastructure/EKS/backend/variables.tf
+++ v1.1/chart/infrastructure/EKS/backend/variables.tf
@@ -2,18 +2,12 @@
 
 variable "project_tag" {
   description = "Project tag to add to s3 and dynamodb names"
-  type        = string
-  default     = "codacy"
+  type = string
+  default = "codacy"
 }
 
 variable "custom_tags" {
   description = "Map of custom tags to apply to every resource"
-  type        = map(string)
-  default     = {}
-}
-
-variable "aws_region" {
-  description = "AWS region where to deploy the infrastructure"
-  type        = string
-  default     = "eu-west-1"
+  type = map(string)
+  default = null
 }
