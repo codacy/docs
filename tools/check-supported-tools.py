358269694dfad622e766bf4e9d2299ef4aae047c
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2021-05-20 11:13:07 +0100

    clean: Make the script executable

--- tools/check-supported-tools.py
+++ tools/check-supported-tools.py
@@ -1,23 +1,26 @@
+#!/usr/bin/env python3
 import argh
+import emoji
 import requests
 
-URL_DOCUMENTATION = "https://docs.codacy.com/getting-started/supported-languages-and-tools/"
-URL_ENDPOINT = "https://api.codacy.com/api/v3/tools"
+DOCUMENTATION_PATH = "../docs/getting-started/supported-languages-and-tools.md"
+ENDPOINT_URL = "https://api.codacy.com/api/v3/tools"
 
 
 def check_supported_tools(verbose=False):
     print("Checking if each tool is included in the documentation:\n")
-    documentation = requests.get(URL_DOCUMENTATION).text.lower()
-    tools = requests.get(URL_ENDPOINT).json()["data"]
+    with open(DOCUMENTATION_PATH, "r") as file:
+        documentation = file.read().lower()
+    tools = requests.get(ENDPOINT_URL).json()["data"]
     count = 0
     for tool in tools:
         tool_name = tool["name"].split(" ")[0]
         tool_short_name = tool["shortName"]
         if tool_name.lower() in documentation or tool_short_name.lower() in documentation:
             if verbose:
-                print(f"✔️  {tool_name} is included")
+                print(emoji.emojize(f":check_mark_button: {tool_name} is included"))
         else:
-            print(f"❌ {tool_name} ISN'T included")
+            print(emoji.emojize(f":cross_mark: {tool_name} ISN'T included"))
             count += 1
     if count:
         print(f"\nFound {count} tools that aren't included in the documentation.")
