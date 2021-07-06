c35aff5ea21b3e18972003c69e279f10812c6667
prcr <prcr@users.noreply.github.com>
2021-07-06 11:33:30 +0000

    Deployed 4ae059166 to v4.1 with MkDocs 1.1.2 and mike 0.5.5

---
+++ v4.1/chart/infrastructure/EKS/backend/state_and_lock.tf
@@ -0,0 +1,50 @@
+# state_and_lock.tf - setup s3 state storage and dynamodb lock table
+#                     For more info see https://www.terraform.io/docs/backends/types/s3.html
+
+resource "aws_s3_bucket" "state" {
+  bucket = "${var.project_tag}-terraform-state-${random_string.rand.result}"
+  acl    = "private"
+
+  ### To destroy uncomment this:
+  #force_destroy = true
+  ##############################
+
+  ### To destroy comment this:
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+  ###########################
+
+  tags = var.custom_tags
+}
+
+resource "aws_dynamodb_table" "lock" {
+  name           = "${var.project_tag}-terraform-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  ### To destroy comment this:
+  lifecycle {
+    prevent_destroy = true
+  }
+  ###########################
+
+  tags = var.custom_tags
+}
+
+resource "random_string" "rand" {
+  length  = 22
+  special = false
+  upper   = false
+  number  = false
+}
