36d92269728047f35374c65409d4c499c3dae822
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2022-01-14 18:46:01 +0000

    fix: Apply Codacy feedback

--- tools/find-orphan-images.sh
+++ tools/find-orphan-images.sh
@@ -2,11 +2,11 @@
 
 DOCUMENTATION_PATH="../docs"
 
-find "$DOCUMENTATION_PATH" -name *.jpg > /tmp/patterns
-find "$DOCUMENTATION_PATH" -name *.png >> /tmp/patterns
-find "$DOCUMENTATION_PATH" -name *.gif >> /tmp/patterns
+find "$DOCUMENTATION_PATH" -name "*.png" > /tmp/patterns
+find "$DOCUMENTATION_PATH" -name "*.gif" >> /tmp/patterns
+find "$DOCUMENTATION_PATH" -name "*.jpg" >> /tmp/patterns
 
 for p in $(cat /tmp/patterns); do
-    f=$(basename $p);
-    grep -R $f "$DOCUMENTATION_PATH" > /dev/null || echo $p;
+    f="$(basename "$p")";
+    grep -R "$f" "$DOCUMENTATION_PATH" > /dev/null || echo "$p";
 done
