2833a4fc27eff29d80eb1135247e5a086e7e22ff
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2022-04-29 12:38:47 +0100

    feature: Add script check-security-tools.py

---
+++ tools/check-security-tools.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import argh
+import emoji
+import requests
+
+from string import Template
+
+DOCUMENTATION_PATH = "../docs/repositories/security-monitor.md"
+ENDPOINT_URL_TOOLS = "https://api.codacy.com/api/v3/tools"
+ENDPOINT_URL_CODE_PATTERNS = Template("https://api.codacy.com/api/v3/tools/${toolUuid}/patterns")
+
+
+def check_security_tools():
+    print("Checking if each tool that detects security issues is included in the documentation:\n")
+    with open(DOCUMENTATION_PATH, "r") as file:
+        documentation = file.read().lower()
+    tools = requests.get(ENDPOINT_URL_TOOLS).json()["data"]
+    count = 0
+    for tool in tools:
+        tool_name = tool["name"].split(" ")[0]
+        tool_short_name = tool["shortName"]
+        code_patterns = requests.get(ENDPOINT_URL_CODE_PATTERNS.substitute(toolUuid=tool["uuid"])).json()["data"]
+        for code_pattern in code_patterns:
+            if code_pattern["category"] == "Security":
+                if tool_name.lower() in documentation or tool_short_name.lower() in documentation:
+                    print(emoji.emojize(f":check_mark_button: {tool_name} is included"))
+                else:
+                    print(emoji.emojize(f":cross_mark: {tool_name} ISN'T included"))
+                    count += 1
+                break
+    if count:
+        print(f"\nFound {count} tools that aren't included in the documentation.")
+
+
+argh.dispatch_command(check_security_tools)
