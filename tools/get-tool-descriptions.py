20df86e5ea20e102646cd0741800cfbbb7ce6d09
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2022-05-04 10:07:17 +0100

    clean: Simplify script output

--- tools/get-tool-descriptions.py
+++ tools/get-tool-descriptions.py
@@ -7,25 +7,20 @@ ENDPOINT_URL = "https://api.codacy.com/api/v3/tools"
 
 
 def get_tool_descriptions(verbose=False):
-    print("Obtaining tool descriptions.\n")
     tools = requests.get(ENDPOINT_URL).json()["data"]
     count = 0
     for tool in tools:
         tool_name = tool["name"]
         tool_description = tool.get("description", "")
         if tool_description == "":
-            print(emoji.emojize(f":cross_mark: {tool_name} DOESN'T have a description"))
             count += 1
-        else:
-            if verbose:
-                print(emoji.emojize(f":check_mark_button: {tool_name} has the description \"{tool_description}\""))
-            else:
-                print(emoji.emojize(f":check_mark_button: {tool_name} has a description"))
+        print(f"## {tool_name}\n"
+              f"{tool_description if tool_description else 'Not available'}\n\n")
     if count:
-        print(f"\nFound {count} tools without description.")
+        print(f"Found {count} tools without description.")
         exit(1)
     else:
-        print(emoji.emojize("\nAll tools have a description! :party_popper:"))
+        print(emoji.emojize("All tools have a description! :party_popper:"))
         exit(0)
 
 
